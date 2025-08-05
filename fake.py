4a769bb3c68c394306ccb8ad2a315acdfc0d50f9
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-05 17:39:52 +0100

    Update fake.py

--- fake.py
+++ fake.py
@@ -1,11 +1,20 @@
 from utils import NewsDataCollector
 
 def main():
-    print(NewsDataCollector().collect_news_data
+    collector = NewsDataCollector()
+    news = collector.collect_news_data("Andrew Tate")
 
+    articles = [
+        i for i in news
+        if 'canadian' in i['title'].lower()]
+
+    if not articles:
+        print("No articles found with 'canadian' in title or summary.")
+    else:
+        for article in articles:
+            print(f"- {article['title']}")
+            print(f"  Source: {article['source']}")
+            print(f"  Published: {article['published']}")
 
 if __name__ == "__main__":
     main()
-    
-    
-collect_news_data(_self, query=None, region='US', category=None, max_articles=50)
