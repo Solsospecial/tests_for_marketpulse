a5ba57e925413d43791abc31840b4913a5032631
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-05 19:04:17 +0000

    done

---
+++ checker.py
@@ -0,0 +1,180 @@
+1
+Title: Viral 'bunnies on a trampoline' video is revealed to be AI - These are the subtle clues that give away the hoax
+Source: Daily Mail
+Sentiment: neutral
+
+
+2
+Title: Stevie Nicks postpones tour dates, AI bunnies on a trampoline and more | What's Trending
+Source: CBS News
+Sentiment: neutral
+
+
+3
+Title: Letting Go of My Diaspora Grief at the Bad Bunny Residency
+Source: The Cut
+Sentiment: neutral
+
+
+4
+Title: The TikTok artist behind viral 'unknowing bunny' song pits human creativity against AI illusion
+Source: Mashable
+Sentiment: neutral
+
+
+5
+Title: The subtle clues that show viral 'bunnies on a trampoline' video is an AI-generated hoax
+Source: MSN
+Sentiment: neutral
+
+
+6
+Title: Let Them Eat Bunny: Zoo Wants You to Donate Your Small Pets
+Source: Newser
+Sentiment: neutral
+
+
+7
+Title: Robot Bunnies Deployed in Florida To Help Fight Massive Python Problem
+Source: vice.com
+Sentiment: neutral
+
+
+8
+Title: 'BLESSED': Music icon Dolly Parton expresses gratitude and opens up about Dollywood
+Source: Fox News
+Sentiment: positive
+
+
+9
+Title: Take a First Look at Bad Bunny’s adidas Adizero SL72
+Source: hypebeast.com
+Sentiment: neutral
+
+
+10
+Title: Enter the Bad Bunny x adidas Archive
+Source: hypebeast.com
+Sentiment: neutral
+
+
+11
+Title: Meet the robo-bunny: This tiny, furry robot is taking on invasive pythons in Florida
+Source: Marco Eagle
+Sentiment: neutral
+
+
+12
+Title: AI slop is ruining all of our favorite places to scroll
+Source: Axios
+Sentiment: negative
+
+
+13
+Title: Opinion | Bad Bunny Just Wants to Stay Home. So Do I.
+Source: The New York Times
+Sentiment: neutral
+
+
+14
+Title: Video really shows rabbits jumping on trampoline?
+Source: Snopes
+Sentiment: neutral
+
+
+15
+Title: Viral “Bunnies on a Trampoline” Video Was AI — Here’s How It Fooled Everyone
+Source: WWLTV.com
+Sentiment: neutral
+
+
+16
+Title: Prairie Life: Keep an Eye on the Bunny
+Source: The New Falcon Herald
+Sentiment: neutral
+
+
+17
+Title: Robot bunnies deployed to fight invasive Burmese Pythons spreading across Florida
+Source: Daily Express US
+Sentiment: neutral
+
+
+18
+Title: How to Spot AI Video, Even If It’s Bunnies
+Source: Vulture
+Sentiment: neutral
+
+
+19
+Title: Shelly Palmer: Bunnies, truthiness, and the AI slop era
+Source: SaskToday.ca
+Sentiment: neutral
+
+
+20
+Title: Bad Bunny Is Bringing F1 to Puerto Rico — Here’s How To Attend
+Source: Remezcla
+Sentiment: neutral
+
+
+21
+Title: What’s Productive About Bunnies Jumping on a Trampoline?
+Source: Bloomberg.com
+Sentiment: neutral
+
+
+22
+Title: Bad Bunny Is Bringing Formula 1 to Puerto Rico, Joins Mercedes-AMG PETRONAS F1 Team
+Source: Billboard
+Sentiment: neutral
+
+
+23
+Title: AI-generated animals in fake surveillance videos are fooling the internet
+Source: Mashable
+Sentiment: negative
+
+
+24
+Title: To Fight Invasive Pythons in the Everglades Scientists Turned to Robot Rabbits
+Source: ZME Science
+Sentiment: neutral
+
+
+25
+Title: People Are Falling for an AI Video of Bunnies Bouncing on a Trampoline
+Source: PetaPixel
+Sentiment: negative
+
+
+26
+Title: Valve dev on bunny hopping bug in CS2: "There's no intent to break bhopping"
+Source: Dust2.us
+Sentiment: neutral
+
+
+27
+Title: AI Bunnies on Trampoline Causing Crisis of Confidence on TikTok
+Source: 404 Media
+Sentiment: neutral
+
+
+28
+Title: Bamboozled: Viral bunny trampoline video is fake
+Source: KSN-TV
+Sentiment: negative
+
+
+29
+Title: 'Bunnies' Take Over Family’s Trampoline Like a Bunch of Kids and It’s Too Cute
+Source: yahoo.com
+Sentiment: positive
+
+
+30
+Title: It's a Bad Bunny summer in Puerto Rico: Ana recaps 2 of his Residency shows : Alt.Latino
+Source: NPR
+Sentiment: positive
+
+
