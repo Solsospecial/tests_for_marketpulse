670d1a6a6cfcf8b9f175059f5c7df8c250c3a121
Solsospecial <solomonilogieneayo@gmail.com>
2025-08-05 18:34:48 +0100

    Update fake.py

--- fake.py
+++ fake.py
@@ -1,6 +1,7 @@
-from app import get_analyzers
+from app import get_analyzers, process_sentiment_analysis
+import pandas as pd
 
-def main():
+def sub_main():
     collector = (get_analyzers()[0])
     news = collector.collect_news_data("Andrew Tate")
 
@@ -9,12 +10,28 @@ def main():
         if 'canadian' in i['title'].lower()]
 
     if not articles:
-        print("No articles found with 'canadian' in title or summary.")
+        # print("No articles found with 'canadian' in title or summary.")
     else:
         for article in articles:
-            print(f"- {article['title']}")
-            print(f"  Source: {article['source']}")
-            print(f"  Published: {article['published']}")
+            # print(f"- {article['title']}")
+            # print(f"  Source: {article['source']}")
+            # print(f"  Published: {article['published']}")
+
+def main():
+    collector = (get_analyzers()[0])
+    news = collector.collect_news_data("Andrew Tate")
+    
+    df = pd.DataFrame(articles)
+    
+    mask = df['title'].str.lower().str.contains('|'.join(['canadian']), na=False)
+    df = df[mask]
+    
+    titles = df['title'].tolist()
+    results = process_sentiment_analysis(titles)
+    
+    print(results)
+        
+
 
 if __name__ == "__main__":
     main()
