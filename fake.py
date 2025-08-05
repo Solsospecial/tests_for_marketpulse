a596fd655ac4be74978c24b93a61d94a0e49b1ff
Solsospec <158485736+Solsospec@users.noreply.github.com>
2025-08-05 14:31:43 +0100

    Update fake.py

--- fake.py
+++ fake.py
@@ -1,1 +1,11 @@
 from utils import NewsDataCollector
+
+def main():
+    print(NewsDataCollector().collect_news_data
+
+
+if __name__ == "__main__":
+    main()
+    
+    
+collect_news_data(_self, query=None, region='US', category=None, max_articles=50)
